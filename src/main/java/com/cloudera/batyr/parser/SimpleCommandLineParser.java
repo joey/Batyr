0db3d6d0ec2fc93606afcca75b7007a5a7b4ae21
Joseph Echeverria <joey@cloudera.com>
2012-05-20 13:42:36 -0700

    Added support for options that don't get consumed with SimpleCommandLineParser.

--- src/main/java/com/cloudera/batyr/parser/SimpleCommandLineParser.java
+++ src/main/java/com/cloudera/batyr/parser/SimpleCommandLineParser.java
@@ -9,22 +9,37 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 
 public class SimpleCommandLineParser {
-  
+
   private static Logger LOG = Logger.getLogger(SimpleCommandLineParser.class);
-  
+
   public static String[] parse(String[] args, String[] options, String[] flags, Map<String, String> settings) {
-    return parse(args, new HashSet<String>(Arrays.asList(options)), new HashSet<String>(Arrays.asList(flags)), settings);
+    return parse(args, options, flags, settings, true);
   }
-  
+
+  public static String[] parse(String[] args, String[] options, String[] flags, Map<String, String> settings, boolean consume) {
+    return parse(args, new HashSet<String>(Arrays.asList(options)), new HashSet<String>(Arrays.asList(flags)), settings, consume);
+  }
+
   public static String[] parse(String[] args, Set<String> options, Set<String> flags, Map<String, String> settings) {
+    return parse(args, options, flags, settings, true);
+  }
+
+  public static String[] parse(String[] args, Set<String> options, Set<String> flags, Map<String, String> settings, boolean consume) {
     List<String> extraArgs = new LinkedList<String>();
-    
+
     for (int idx = 0; idx < args.length; idx++) {
       if (args[idx].startsWith("-")) {
         String opt = args[idx].replaceFirst("-*", "");
         if (options.contains(opt)) {
+          if (!consume) {
+            extraArgs.add(args[idx]);
+            extraArgs.add(args[idx + 1]);
+          }
           settings.put(opt, args[++idx]);
         } else if (flags.contains(opt)) {
+          if (!consume) {
+            extraArgs.add(args[idx]);
+          }
           settings.put(opt, Boolean.toString(true));
         } else {
           extraArgs.add(args[idx]);
@@ -33,7 +48,7 @@ public class SimpleCommandLineParser {
         extraArgs.add(args[idx]);
       }
     }
-    
+
     return extraArgs.toArray(new String[extraArgs.size()]);
   }
 }
